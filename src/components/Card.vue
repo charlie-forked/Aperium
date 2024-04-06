a287b5b40e87a67808f83d39b93f686137289e3d
Butwm <78359657+Butwm@users.noreply.github.com>
2024-04-06 09:54:25 +0200

    update

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+	<li class="user-card list-none bg-[#0d0d12] relative border-none text-white">
+	  <a target="_blank" :href="href" class="flex items-center">
+		<img class="w-12 h-12 rounded-full" :src="image" alt="Card Image" v-if="image" />
+		<div class="ml-4 flex flex-col">
+		  <h2 class="text-[14px] font-bold">{{ title }}</h2>
+		  <p class="text-[11px] font-normal">{{ role }}</p>
+		  <p class="text-[13px] font-normal">{{ body }}</p>
+		</div>
+	  </a>
+	</li>
+  </template>
+  
+  <script>
+  export default {
+	props: {
+	  href: String,
+	  title: String,
+	  body: String,
+	  image: String,
+	  role: String
+	}
+  };
+  </script>
---
+++ src/components/CardSlider.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container w-full">
+      <div class="moving-element flex justify-between">
+        <Card 
+          href="https://google.com"
+          title="Card Title"
+          role="CEO"
+          body="Content of the card."
+          image="https://cdn.discordapp.com/avatars/795996972758204426/1fedd8ab05818df52c73fc621bb8f816?size=1024"
+        />
+        <Card 
+          href="https://google.com"
+          title="Card Title"
+          role="CEO"
+          body="Content of the card."
+          image="https://cdn.discordapp.com/avatars/795996972758204426/1fedd8ab05818df52c73fc621bb8f816?size=1024"
+        /> 
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Card from './Card.vue';
+  
+  export default {
+    components: {
+      Card,
+    },
+  };
+  </script>
+  
+  <style>
+  .container {
+    position: relative;
+    width: 100%;
+    height: 200px;
+    overflow: hidden;
+    margin-right: 25px;
+    padding-right: 25px;
+  }
+  
+  .moving-element {
+    position: absolute;
+    top: 50%;
+    width: 210px;
+    height: 50px;
+    right: 0;
+    background-color: rgb(126, 126, 231);
+    animation: moveRightToLeft 10s linear infinite;
+    display: flex;
+  }
+
+  .moving-element > * {
+    
+    padding-right: 20px; 
+}
+  
+  @keyframes moveRightToLeft {
+    0% {
+      right: 0;
+    }
+    100% {
+      right: 100%;
+    }
+  }
+  </style>
+  
