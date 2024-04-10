01d860cf0507a44b7e9f836272d0e7155e47641f
Butwm <78359657+Butwm@users.noreply.github.com>
2024-04-10 18:57:56 +0200

    update - pricing added

--- src/components/cardslider/Card.vue
+++ src/components/cardslider/Card.vue
@@ -1,5 +1,5 @@
 <template>
-	<li class="user-card list-none bg-[#d1ced1aa] dark:bg-[#0d0d12] relative border-none text-white">
+	<li class="user-card list-none mt-12 bg-[#d1ced1aa] dark:bg-[#0d0d12] relative border-none text-white">
 	  <a target="_blank" :href="href" class="flex items-center">
 		<img class="w-12 h-12 rounded-full" :src="image" alt="Card Image" v-if="image" />
 		<div class="ml-4 flex flex-col">
--- src/components/sections/OurProgram.vue
+++ src/components/sections/OurProgram.vue
@@ -1,44 +1,44 @@
 <template class="">
-    <div class="mt-20 w-full h-full flex flex-col justify-center items-center">
+    <div class="mt-20 mb-24 w-full h-full flex flex-col justify-center items-center">
         <h2 class="text-xl font-black bg-gradient-to-r from-[#8b9ce1] via-[#a88aef] to-[#dc75fc] text-transparent bg-clip-text">Our Program</h2>
         <h1 class="mt-4 text-5xl font-bold text-white">How it Works?</h1>
-        <div id="items" class="flex flex-row mt-12 ml-2 mb-20 justify-center items-center relative"> <!-- Updated class to flex-row -->
+        <div id="items" class="mt-12 ml-2 mb-20 justify-center items-center relative"> <!-- Removed flex flex-row class -->
             <div class="w-[710px] my-auto h-[320px] flex z-8 justify-center rounded-xl border border-gray-700 bg-[#0d0d12] items-center">
-              <div class="flex flex-row justify-center items-center"> 
-                <div class="flex my-auto absolute top-[60px]">  
-                    <div class="flex w-[295px] my-auto">
-                        <svg class="w-14" xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M14.82 4.26a10.14 10.14 0 0 0-.53 1.1 14.66 14.66 0 0 0-4.58 0 10.14 10.14 0 0 0-.53-1.1 16 16 0 0 0-4.13 1.3 17.33 17.33 0 0 0-3 11.59 16.6 16.6 0 0 0 5.07 2.59A12.89 12.89 0 0 0 8.23 18a9.65 9.65 0 0 1-1.71-.83 3.39 3.39 0 0 0 .42-.33 11.66 11.66 0 0 0 10.12 0q.21.18.42.33a10.84 10.84 0 0 1-1.71.84 12.41 12.41 0 0 0 1.08 1.78 16.44 16.44 0 0 0 5.06-2.59 17.22 17.22 0 0 0-3-11.59 16.09 16.09 0 0 0-4.09-1.35zM8.68 14.81a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.93 1.93 0 0 1 1.8 2 1.93 1.93 0 0 1-1.8 2zm6.64 0a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.92 1.92 0 0 1 1.8 2 1.92 1.92 0 0 1-1.8 2z"></path></svg>
-                        <div class="ml-4 flex flex-col">
-                            <a href="discord.com" target="_blank"><h2 class="text-[13px] dark:hover:text-[#8893f3] transition-colors duration-1000 ease-linear text-[#d3d3d3] font-bold flex">Join Discord</h2></a>
-                            <p class="text-[14px] font-regular text-[#d3d3d3]/90">Join our discord where everything starts</p>
+                <div class="flex justify-center items-center"> 
+                    <div class="flex my-auto absolute top-[60px]">  
+                        <div class="flex w-[295px] my-auto">
+                            <svg class="w-14" xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M14.82 4.26a10.14 10.14 0 0 0-.53 1.1 14.66 14.66 0 0 0-4.58 0 10.14 10.14 0 0 0-.53-1.1 16 16 0 0 0-4.13 1.3 17.33 17.33 0 0 0-3 11.59 16.6 16.6 0 0 0 5.07 2.59A12.89 12.89 0 0 0 8.23 18a9.65 9.65 0 0 1-1.71-.83 3.39 3.39 0 0 0 .42-.33 11.66 11.66 0 0 0 10.12 0q.21.18.42.33a10.84 10.84 0 0 1-1.71.84 12.41 12.41 0 0 0 1.08 1.78 16.44 16.44 0 0 0 5.06-2.59 17.22 17.22 0 0 0-3-11.59 16.09 16.09 0 0 0-4.09-1.35zM8.68 14.81a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.93 1.93 0 0 1 1.8 2 1.93 1.93 0 0 1-1.8 2zm6.64 0a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.92 1.92 0 0 1 1.8 2 1.92 1.92 0 0 1-1.8 2z"></path></svg>
+                            <div class="ml-4 flex flex-col">
+                                <a href="discord.com" target="_blank"><h2 class="text-[13px] dark:hover:text-[#8893f3] transition-colors duration-1000 ease-linear text-[#d3d3d3] font-bold flex">Join Discord</h2></a>
+                                <p class="text-[14px] font-regular text-[#d3d3d3]/90">Join our discord where everything starts</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="flex w-[295px] my-auto">
-                        <svg class="w-14" xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M20 10.999h2C22 5.869 18.127 2 12.99 2v2C17.052 4 20 6.943 20 10.999z"></path><path d="M13 8c2.103 0 3 .897 3 3h2c0-3.225-1.775-5-5-5v2zm3.422 5.443a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a1 1 0 0 0-.086-1.391l-4.064-3.696z"></path></svg>
-                        <div class="ml-4 flex flex-col">
-                            <h2 class="cursor-pointer text-[13px] transition-colors duration-1000 ease-linear text-[#d3d3d3] font-bold flex">Join Free Calls</h2>
-                            <p class="text-[14px] font-regular text-[#d3d3d3]/90">Every Week we have 1-2 Calls about Business.</p>
+                        <div class="flex w-[295px] my-auto">
+                            <svg class="w-14" xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M20 10.999h2C22 5.869 18.127 2 12.99 2v2C17.052 4 20 6.943 20 10.999z"></path><path d="M13 8c2.103 0 3 .897 3 3h2c0-3.225-1.775-5-5-5v2zm3.422 5.443a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a1 1 0 0 0-.086-1.391l-4.064-3.696z"></path></svg>
+                            <div class="ml-4 flex flex-col">
+                                <h2 class="cursor-pointer text-[13px] transition-colors duration-1000 ease-linear text-[#d3d3d3] font-bold flex">Join Free Calls</h2>
+                                <p class="text-[14px] font-regular text-[#d3d3d3]/90">Every Week we have 1-2 Calls about Business.</p>
+                            </div>
+                        </div> 
+                    </div>    
+                    <div class="flex mt-[140px] mb-[20px] my-auto absolute">  
+                        <div class="flex w-[295px] my-auto">
+                            <svg class="w-14" xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M4 8H2v12a2 2 0 0 0 2 2h12v-2H4z"></path><path d="M20 2H8a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm-9 12V6l7 4z"></path></svg>
+                            <div class="ml-4 flex flex-col">
+                                <h2 class="cursor-pointer text-[13px] transition-colors duration-1000 ease-linear text-[#d3d3d3] font-bold flex">Watch Our Videos</h2>
+                                <p class="text-[14px] font-regular text-[#d3d3d3]/90">Watch our Mentor-ship Videos.</p>                
+                            </div>
                         </div>
-                    </div> 
-                </div>    
-                <div class="flex mt-[140px] mb-[20px] my-auto absolute">  
-                    <div class="flex w-[295px] my-auto">
-                        <svg class="w-14" xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M4 8H2v12a2 2 0 0 0 2 2h12v-2H4z"></path><path d="M20 2H8a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm-9 12V6l7 4z"></path></svg>
-                        <div class="ml-4 flex flex-col">
-                            <h2 class="cursor-pointer text-[13px] transition-colors duration-1000 ease-linear text-[#d3d3d3] font-bold flex">Watch Our Videos</h2>
-                            <p class="text-[14px] font-regular text-[#d3d3d3]/90">Watch our Mentor-ship Videos.</p>                
+                        <div class="flex w-[295px] my-auto">
+                            <svg class="w-14" xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M4 21h1V8H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2zM20 8h-7l1.122-3.368A2 2 0 0 0 12.225 2H12L7 7.438V21h11l3.912-8.596L22 12v-2a2 2 0 0 0-2-2z"></path></svg>
+                            <div class="ml-4 flex flex-col">
+                                <h2 class="cursor-pointer text-[13px] transition-colors duration-1000 ease-linear text-[#d3d3d3] font-bold flex">Send Your Results</h2>
+                                <p class="text-[14px] font-regular text-[#d3d3d3]/90">Send your results to our Discord Channel.</p>                
+                            </div>
                         </div>
-                    </div>
-                    <div class="flex w-[295px] my-auto">
-                        <svg class="w-14" xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M4 21h1V8H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2zM20 8h-7l1.122-3.368A2 2 0 0 0 12.225 2H12L7 7.438V21h11l3.912-8.596L22 12v-2a2 2 0 0 0-2-2z"></path></svg>
-                        <div class="ml-4 flex flex-col">
-                            <h2 class="cursor-pointer text-[13px] transition-colors duration-1000 ease-linear text-[#d3d3d3] font-bold flex">Send Your Results</h2>
-                            <p class="text-[14px] font-regular text-[#d3d3d3]/90">Send your results to our Discord Channel.</p>                
-                        </div>
-                    </div>
-                </div>                            
+                    </div>                            
+                </div>
             </div>
-          </div> 
         </div> 
     </div>
 </template>
